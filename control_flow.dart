fbf636305e3dd369f3c472db08e305d515111e3b
micael20 <159913366+micael20@users.noreply.github.com>
2024-03-11 00:01:15 +0300

    first commit

---
+++ control_flow.dart
@@ -0,0 +1,16 @@
+import 'dart:io';
+
+void main() {
+  // Prompting the user for a number
+  stdout.write('Enter a number: ');
+  int number = int.parse(stdin.readLineSync()!);
+
+  // Checking the number and printing the message
+  if (number > 10) {
+    print("Your number is greater than 10");
+  } else if (number < 10) {
+    print("Your number is less than 10");
+  } else {
+    print("Your number is equal to 10");
+  }
+}
